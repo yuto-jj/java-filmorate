66d60cb653fe19987fa583f9c73d95ddefac7202
yuto-jj <r.5ilence@yandex.ru>
2024-10-20 11:30:37 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -7,9 +7,9 @@ import java.time.LocalDate;
 @Data
 @Builder
 public class User {
-    Long id;
-    String email;
-    String login;
-    String name;
-    LocalDate birthday;
+    private Long id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
 }
